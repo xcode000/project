198b9a94e5a90b4ef7f4ce03f887255beb2cf642
xCode <106350325+xcode000@users.noreply.github.com>
2025-08-09 05:49:14 +0700

    Up

--- ping/cek-noobz.sh
+++
@@ -1,2 +0,0 @@
-noobzvpns --info-all-user | awk '/^\s*\+.*-> active/ {gsub(/(issued\(yyyymmdd\)|hash_key): [0-9a-f]+/, ""); print; getline; print; getline; getline; getline; print; print "═══════════════════"}'
-
